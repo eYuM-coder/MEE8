d4a009ccff2c60decbadc47c0b0168cebd23c12b
eYuM-coder <nathaniel2007w@gmail.com>
2024-08-30 17:28:51 -0600

    a

--- src/slashCommands/fun/dmoptin.js
+++ src/slashCommands/fun/dmoptin.js
@@ -4,52 +4,52 @@ const { MessageEmbed } = require("discord.js");
 const dmSystem = require("../../database/models/dmSystem");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("dmoptout")
-        .setDescription("Opts out of the DM command"),
-    async execute(interaction) {
-        try {
-            const client = interaction.client;
-            const logging = await Logging.findOne({ guildId: interaction.guild.id });
+  data: new SlashCommandBuilder()
+    .setName("dmoptout")
+    .setDescription("Opts out of the DM command"),
+  async execute(interaction) {
+    try {
+      const client = interaction.client;
+      const logging = await Logging.findOne({ guildId: interaction.guild.id });
 
-            const dmsystem = await dmSystem.findOne({ userId: interaction.author.id });
-            if (!dmsystem) {
-                await new dmSystem({
-                    userId: interaction.author.id,
-                    optedout: true,
-                });
-                const dmoptin = new MessageEmbed()
-                    .setDescription(`You have opted in to the DM system. To stop recieving DMs, use /dmoptout.`);
-                return interaction.reply({ embeds: [dmoptin] })
-                    .then(async () => {
-                        if (logging && logging.moderation.delete_reply === "true") {
-                            setTimeout(() => {
-                                interaction.deleteReply().catch(() => { interaction.channel.send({ content: "Error deleting message." }) });
-                            }, 5000)
-                        }
-                    })
-                    .catch(() => { interaction.channel.send({ content: "Error deleting message." }) });
-            } else {
-                await dmSystem.updateOne(
-                    {
-                        userId: message.author.id,
-                    },
-                    { $set: { optedout: false } }
-                );
-                const dmoptin = new MessageEmbed()
-                    .setDescription(`You have opted in to use the DM system. To stop recieving DMs, use /dmoptout.`);
-                return interaction.reply({ embeds: [dmoptin] })
-                    .then(async () => {
-                        if (logging && logging.moderation.delete_reply === "true") {
-                            setTimeout(() => {
-                                interaction.deleteReply().catch(() => { interaction.channel.send({ content: "Error deleting message." }) });
-                            }, 5000)
-                        }
-                    })
-                    .catch(() => { interaction.channel.send({ content: "Error deleting message." }) });
+      const dmsystem = await dmSystem.findOne({ userId: interaction.author.id });
+      if (!dmsystem) {
+        await new dmSystem({
+          userId: interaction.author.id,
+          optedout: true,
+        });
+        const dmoptin = new MessageEmbed()
+          .setDescription(`You have opted in to the DM system. To stop recieving DMs, use /dmoptout.`);
+        return interaction.reply({ embeds: [dmoptin] })
+          .then(async () => {
+            if (logging && logging.moderation.delete_reply === "true") {
+              setTimeout(() => {
+                interaction.deleteReply().catch(() => { interaction.channel.send({ content: "Error deleting message." }) });
+              }, 5000)
             }
-        } catch (err) {
-            console.error();
-        }
+          })
+          .catch(() => { interaction.channel.send({ content: "Error deleting message." }) });
+      } else {
+        await dmSystem.updateOne(
+          {
+            userId: message.author.id,
+          },
+          { $set: { optedout: false } }
+        );
+        const dmoptin = new MessageEmbed()
+          .setDescription(`You have opted in to use the DM system. To stop recieving DMs, use /dmoptout.`);
+        return interaction.reply({ embeds: [dmoptin] })
+          .then(async () => {
+            if (logging && logging.moderation.delete_reply === "true") {
+              setTimeout(() => {
+                interaction.deleteReply().catch(() => { interaction.channel.send({ content: "Error deleting message." }) });
+              }, 5000)
+            }
+          })
+          .catch(() => { interaction.channel.send({ content: "Error deleting message." }) });
+      }
+    } catch (err) {
+      console.error();
     }
+  }
 };
--- src/slashCommands/fun/dmoptout.js
+++ src/slashCommands/fun/dmoptout.js
@@ -4,52 +4,52 @@ const { MessageEmbed } = require("discord.js");
 const dmSystem = require("../../database/models/dmSystem");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("dmoptout")
-        .setDescription("Opts out of the DM command"),
-    async execute(interaction) {
-        try {
-            const client = interaction.client;
-            const logging = await Logging.findOne({ guildId: interaction.guild.id });
+  data: new SlashCommandBuilder()
+    .setName("dmoptout")
+    .setDescription("Opts out of the DM command"),
+  async execute(interaction) {
+    try {
+      const client = interaction.client;
+      const logging = await Logging.findOne({ guildId: interaction.guild.id });
 
-            const dmsystem = await dmSystem.findOne({ userId: interaction.author.id });
-            if (!dmsystem) {
-                await new dmSystem({
-                    userId: interaction.author.id,
-                    optedout: true,
-                });
-                const dmoptout = new MessageEmbed()
-                    .setDescription(`You have opted out of the DM system. To begin recieving DMs again, use /dmoptin.`);
-                return interaction.reply({ embeds: [dmoptout] })
-                    .then(async () => {
-                        if (logging && logging.moderation.delete_reply === "true") {
-                            setTimeout(() => {
-                                interaction.deleteReply().catch(() => { interaction.channel.send({ content: "Error deleting message." }) });
-                            }, 5000)
-                        }
-                    })
-                    .catch(() => { interaction.channel.send({ content: "Error deleting message." }) });
-            } else {
-                await dmSystem.updateOne(
-                    {
-                        userId: message.author.id,
-                    },
-                    { $set: { optedout: true } }
-                );
-                const dmoptout = new MessageEmbed()
-                    .setDescription(`You have opted out of the DM system. To begin recieving DMs again, use /dmoptin.`);
-                return interaction.reply({ embeds: [dmoptout] })
-                    .then(async () => {
-                        if (logging && logging.moderation.delete_reply === "true") {
-                            setTimeout(() => {
-                                interaction.deleteReply().catch(() => { interaction.channel.send({ content: "Error deleting message." }) });
-                            }, 5000)
-                        }
-                    })
-                    .catch(() => { interaction.channel.send({ content: "Error deleting message." }) });
+      const dmsystem = await dmSystem.findOne({ userId: interaction.author.id });
+      if (!dmsystem) {
+        await new dmSystem({
+          userId: interaction.author.id,
+          optedout: true,
+        });
+        const dmoptout = new MessageEmbed()
+          .setDescription(`You have opted out of the DM system. To begin recieving DMs again, use /dmoptin.`);
+        return interaction.reply({ embeds: [dmoptout] })
+          .then(async () => {
+            if (logging && logging.moderation.delete_reply === "true") {
+              setTimeout(() => {
+                interaction.deleteReply().catch(() => { interaction.channel.send({ content: "Error deleting message." }) });
+              }, 5000)
             }
-        } catch (err) {
-            console.error();
-        }
+          })
+          .catch(() => { interaction.channel.send({ content: "Error deleting message." }) });
+      } else {
+        await dmSystem.updateOne(
+          {
+            userId: message.author.id,
+          },
+          { $set: { optedout: true } }
+        );
+        const dmoptout = new MessageEmbed()
+          .setDescription(`You have opted out of the DM system. To begin recieving DMs again, use /dmoptin.`);
+        return interaction.reply({ embeds: [dmoptout] })
+          .then(async () => {
+            if (logging && logging.moderation.delete_reply === "true") {
+              setTimeout(() => {
+                interaction.deleteReply().catch(() => { interaction.channel.send({ content: "Error deleting message." }) });
+              }, 5000)
+            }
+          })
+          .catch(() => { interaction.channel.send({ content: "Error deleting message." }) });
+      }
+    } catch (err) {
+      console.error();
     }
+  }
 };

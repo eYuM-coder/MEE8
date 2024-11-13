d00c1d9c6271aab8d2781b5e7a6d2cfc8aa0d155
Nathaniel Wood <woodnathnaiel@lcps.net>
2024-11-13 14:24:31 -0700

    a

---
+++ src/slashCommands/economy/addmoney.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+const Profile = require("../../database/models/economy/profile");
+const { createProfile } = require("../../utils/utils.js");
+const { execute } = require("../fun/dm.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("addmoney")
+    .setDescription("Add money to a users wallet.")
+    .addUserOption((option) => option.setName("member").setDescription("The member").setRequired(true))
+    .addIntegerOption((option) => option.setName("amount").setDescription("The amount to add").setRequired(true)),
+  async execute(interaction) {
+    const user = interaction.options.getMember("member");
+    const amount = interaction.options.getInteger("amount");
+    const profile = await Profile.findOne({ userID: user.id, guildId: interaction.guild.id });
+    if (!profile) {
+      await createProfile(user, interaction.guild);
+      await interaction.reply({
+        embeds: [
+          new MessageEmbed()
+            .setColor("BLURPLE")
+            .setDescription(`Creating profile.\nUse this command again to use it.`)
+        ], ephemeral: true
+      });
+    } else {
+      await Profile.updateOne({
+        userID: user.id, guildId: interaction.guild.id
+      }, { $inc: { wallet: amount } });
+      await interaction.reply({
+        embeds: [
+          new MessageEmbed()
+            .setColor("BLURPLE")
+            .setDescription(`Added $${amount} to ${user}`)
+        ]
+      });
+    }
+  }
+};
---
+++ src/slashCommands/economy/balance.js
@@ -0,0 +1,40 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+const Profile = require("../../database/models/economy/profile");
+const { createProfile } = require("../../utils/utils");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("balance")
+    .setDescription("Check your balance or the balance of another user")
+    .addUserOption((option) => option.setName("member").setDescription("The optional member to check")),
+    async execute(interaction) {
+        const user = interaction.options.getMember("member");
+
+        const profile = await Profile.findOne({
+            userID: user.id,
+            guildId: interaction.guild.id
+        });
+        if (!profile) {
+            if(user.id !== interaction.user.id) return interaction.reply({ content: `${user} does not have a profile!`, ephemeral: true });
+
+            await createProfile(user, interaction.guild);
+            await interaction.reply({
+                embeds: [
+                    new MessageEmbed()
+                    .setColor("BLURPLE")
+                    .setDescription(`Creating profile.\nUse this command again to check your balance.`)
+                ], ephemeral: true
+            });
+        } else {
+            await interaction.reply({
+                embeds: [
+                    new MessageEmbed()
+                    .setColor("BLURPLE")
+                    .setTitle(`${user.username}'s Balance`)
+                    .setDescription(`**Wallet:** $${profile.wallet}\n**Bank:** $${profile.bank}`)
+                ]
+            });
+        }
+    }
+};
